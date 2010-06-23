639e708db62569ca1c6a8f909fc66e34d9ad0528
Larry Staton Jr <larry@sweetpeasoftware.com>
2010-06-23 09:47:14 -0400

    Added test for performSelector:withObject:

--- test_dynamic_methods.rb
+++ test_dynamic_methods.rb
@@ -17,6 +17,10 @@ class DynamicMethodTest < Test::Unit::TestCase
     assert_equal 6, @obj.send(:mymethod, 3)
   end
   
+  def test_perform_selector
+    assert_equal 6, @obj.performSelector(:"mymethod:", :withObject => 3)
+  end
+  
   def test_define_method
     assert_equal 9, @obj.dynamic(3)
   end
