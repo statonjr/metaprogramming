b4b40acb24a0c18419bc208b924a9024f5c0642d
Larry Staton Jr <larry@sweetpeasoftware.com>
2010-06-24 16:05:52 -0400

    Add the test for clean_room

--- test_clean_room.rb
+++ test_clean_room.rb
@@ -5,6 +5,6 @@ require "clean_room.rb"
 
 class TestCleanRoom < Test::Unit::TestCase
   def test_instance_eval
-    
+    assert_equal('Doing something...', @obj.instance_eval { do_something })
   end
 end
